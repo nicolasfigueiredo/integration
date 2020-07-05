7641ae767314ade2a0d0dcd4d0c2524344951ac5
Nicolas Figueiredo <nicosfigueiredo@gmail.com>
2020-07-05 13:50:55 -0300

    first results cost experiment

---
+++ scripts/mauricio_solutions/lukin_todd.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sparsity
+
+def lukin_todd(specs, kernel_size, eta=1):
+    """
+    Gets a list of spectrograms (normalized, interpolated) and combines them
+    according to the Lukin & Todd method
+    """
+    window = np.ones(kernel_size)
+    sparsities = sparsity.get_sparsities(specs, window, mode='lukin todd')
+    multires_spec = np.sum(specs * (sparsities ** eta) / np.sum((sparsities ** eta)), axis=0)
+    return multires_spec
